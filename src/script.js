36eb9283aa7de33ee48b4704edca9786d110964f
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-20 17:48:06 +0300

    Created class Task and module to operate tasks array

--- src/script.js
+++ src/script.js
@@ -1,1 +1,29 @@
 import "./style.css";
+
+class Task {
+  constructor(
+    title,
+    description,
+    // dueDate,
+    // priority,
+    // notes,
+    checklist
+  ) {
+    this.title = title;
+    this.description = description;
+    // this.dueDate = dueDate;
+    // this.priority = priority;
+    // this.notes = notes;
+    this.checklist = checklist;
+  }
+}
+
+const AllTasks = function () {
+  const tasks = [];
+
+  const addTask = (task) => tasks.push(task);
+  const removeTask = (task) => tasks.splice(index, 1);
+  const getTasks = () => [...tasks];
+
+  return { addTask, removeTask, getTasks };
+};
