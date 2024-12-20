199433305a1c94e00345b72c358e0119d9550c80
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-20 18:50:54 +0300

    Created first draft of DOM controller

--- src/script.js
+++ src/script.js
@@ -27,3 +27,38 @@ const AllTasks = function () {
 
   return { addTask, removeTask, getTasks };
 };
+
+const DisplayTaskController = function () {
+  const renderTasks = function () {
+    const taskContainer = document.getElementById("task-container");
+    taskContainer.innerHTML = "";
+
+    // display tasks
+    AllTasks.getTasks().forEach((task) => {
+      const taskCard = document.createElement("div");
+      taskCard.classList.add("task-card");
+
+      const taskTitle = document.createElement("h2");
+      taskTitle.classList.add("task-title");
+      taskTitle.textContent = task.title;
+
+      const taskDescription = document.createElement("p");
+      taskDescription.classList.add("task-description");
+      taskDescription.textContent = task.description;
+
+      const taskChecklist = document.createElement("input");
+      taskChecklist.setAttribute("type", "checkbox");
+
+      taskCard.appendChild(taskTitle);
+      taskCard.appendChild(taskDescription);
+      taskCard.appendChild(taskChecklist);
+
+      taskContainer.appendChild(taskCard);
+    });
+  };
+
+  return { renderTasks };
+};
+
+const showTest = DisplayTaskController.renderTasks();
+showTest();
