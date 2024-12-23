b9c050ab2eaea9d4d8355e54a92f21674ce314e5
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-23 16:16:43 +0300

    Added functionality to Add Task button
    Added dialog after click on Add Task in js. Created dialog in HTML and added style in CSS

--- src/script.js
+++ src/script.js
@@ -90,6 +90,27 @@ const DisplayTaskController = function () {
   return { renderTasks };
 };
 
+// Add event listener for add task button
+const newTaskButton = document.getElementById("add-task");
+const addTaskDialog = document.getElementById("add-task-dialog");
+const confirmAddButton = document.getElementById("confirm-add");
+
+newTaskButton.addEventListener("click", () => {
+  addTaskDialog.showModal();
+});
+
+confirmAddButton.addEventListener("click", () => {
+  const title = document.getElementById("task-title").value;
+  const description = document.getElementById("task-desc").value;
+
+  if (title && description) {
+    const addingNewTask = new Task(title, description, false);
+    AllTasks.addTask(addingNewTask);
+    const taskController = DisplayTaskController();
+    taskController.renderTasks();
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   AllTasks.startWithTestTasks();
   const taskController = DisplayTaskController();
