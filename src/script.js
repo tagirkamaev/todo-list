6454f45c467369a26acb01fa53ebf9f3f2a8add4
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-23 14:15:03 +0300

    Updated style
    New style is implementing tasks in line design

--- src/script.js
+++ src/script.js
@@ -42,23 +42,35 @@ const DisplayTaskController = function () {
     // display tasks
     AllTasks.getTasks().forEach((task, index) => {
       const taskCard = document.createElement("div");
-      taskCard.classList.add("task-card");
+      taskCard.classList.add("todo");
 
-      const taskTitle = document.createElement("h2");
-      taskTitle.classList.add("task-title");
-      taskTitle.textContent = task.title;
-
-      const taskDescription = document.createElement("p");
-      taskDescription.classList.add("task-description");
-      taskDescription.textContent = task.description;
-
+      // checkbox
+      const checkbox = document.createElement("div");
+      checkbox.classList.add("checkbox");
       const taskChecklist = document.createElement("input");
       taskChecklist.setAttribute("type", "checkbox");
       taskChecklist.checked = task.checklist;
       taskChecklist.addEventListener("change", () => {
         task.checklist = taskChecklist.checked;
       });
+      checkbox.appendChild(taskChecklist);
 
+      // task content
+      const taskContent = document.createElement("div");
+      taskContent.classList.add("task-content");
+
+      const taskTitle = document.createElement("span");
+      taskTitle.classList.add("task-title");
+      taskTitle.textContent = task.title;
+
+      const taskDescription = document.createElement("span");
+      taskDescription.classList.add("task-desc");
+      taskDescription.textContent = task.description;
+
+      taskContent.appendChild(taskTitle);
+      taskContent.appendChild(taskDescription);
+
+      // delete button
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "Delete";
       deleteButton.classList.add("delete-task");
@@ -67,9 +79,8 @@ const DisplayTaskController = function () {
         renderTasks();
       });
 
-      taskCard.appendChild(taskTitle);
-      taskCard.appendChild(taskDescription);
-      taskCard.appendChild(taskChecklist);
+      taskCard.appendChild(checkbox);
+      taskCard.appendChild(taskContent);
       taskCard.appendChild(deleteButton);
 
       taskContainer.appendChild(taskCard);
