ffa97ec760cdc164e57e71ce83f4091ca03f6a4f
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-23 17:48:05 +0300

    Moved logic from script to AppController
    Now AddTask logics is in AppController. Isolated AddEventListeners as a function; created handleAddTask. Now script only initializes app

--- src/AppController.js
+++ src/AppController.js
@@ -3,11 +3,23 @@ import DisplayTaskController from "./DisplayTaskController";
 
 const AppController = (function () {
   const initialize = () => {
+    // rendering example tasks
     AllTasks.startWithTestTasks();
+
+    // callbacks for display and logic interaction
     DisplayTaskController.setCallbacks({
       onDelete: handleDeleteTask,
       onToggle: handleToggleTask,
     });
+
+    setupEventListeners();
+
+    updateUI();
+  };
+
+  const handleAddTask = (title, description) => {
+    const newTask = { title, description, checklist: false };
+    AllTasks.addTask(newTask);
     updateUI();
   };
 
@@ -26,6 +38,27 @@ const AppController = (function () {
   const updateUI = () => {
     const tasks = AllTasks.getTasks();
     DisplayTaskController.renderTasks(tasks);
+  };
+
+  const setupEventListeners = () => {
+    const newTaskButton = document.getElementById("add-task");
+    const addTaskDialog = document.getElementById("add-task-dialog");
+    const confirmAddButton = document.getElementById("confirm-add");
+
+    newTaskButton.addEventListener("click", () => {
+      addTaskDialog.showModal();
+    });
+
+    confirmAddButton.addEventListener("click", () => {
+      const title = document.getElementById("task-title").value;
+      const description = document.getElementById("task-desc").value;
+
+      if (title && description) {
+        handleAddTask(title, description);
+      }
+
+      addTaskDialog.close();
+    });
   };
 
   return { initialize };
--- src/script.js
+++ src/script.js
@@ -1,29 +1,5 @@
 import "./style.css";
-import AllTasks from "./AllTasks";
-import DisplayTaskController from "./DisplayTaskController";
-import Task from "./task";
 import AppController from "./AppController";
-
-// Add event listener for add task button
-const newTaskButton = document.getElementById("add-task");
-const addTaskDialog = document.getElementById("add-task-dialog");
-const confirmAddButton = document.getElementById("confirm-add");
-
-newTaskButton.addEventListener("click", () => {
-  addTaskDialog.showModal();
-});
-
-confirmAddButton.addEventListener("click", () => {
-  const title = document.getElementById("task-title").value;
-  const description = document.getElementById("task-desc").value;
-
-  if (title && description) {
-    const addingNewTask = new Task(title, description, false);
-    AllTasks.addTask(addingNewTask);
-    const taskController = DisplayTaskController();
-    taskController.renderTasks();
-  }
-});
 
 document.addEventListener("DOMContentLoaded", () => {
   AppController.initialize();
