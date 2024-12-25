434e08c867994d6bbfd06d0231864cb69b885fac
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-25 16:43:24 +0300

    Added filter methods
    Added methods to filter tasks depending on dueDate (overdue, today, this week) using date-fns library

--- src/AllTasks.js
+++ src/AllTasks.js
@@ -1,4 +1,8 @@
 import Task from "./task";
+import isBefore from "date-fns/isBefore";
+import isToday from "date-fns/isToday";
+import isWithinInterval from "date-fns/isWithinInterval";
+import addDays from "date-fns/addDays";
 
 const AllTasks = (function () {
   const tasks = [];
@@ -13,6 +17,31 @@ const AllTasks = (function () {
     if (tasks[index]) {
       tasks[index].dueDate = newDueDate;
     }
+  };
+
+  // filtering methods
+  const getOverdueTasks = () => {
+    const currentDate = new Date();
+    return tasks.filter(
+      (task) => task.dueDate && isBefore(new Date(task.dueDate), currentDate)
+    );
+  };
+  const getTodayTasks = () => {
+    return tasks.filter(
+      (task) => task.dueDate && isToday(new Date(task.dueDate))
+    );
+  };
+  const getThisWeekTasks = () => {
+    const today = new Date();
+    const endOfWeek = addDays(today, 7);
+
+    return tasks.filter((task) => {
+      if (!task.dueDate) return false;
+      return isWithinInterval(new Date(task.dueDate), {
+        start: today,
+        end: endOfWeek,
+      });
+    });
   };
 
   const startWithTestTasks = () => {
