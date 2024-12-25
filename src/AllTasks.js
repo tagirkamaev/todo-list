b81cc1b1e9078cf4a075eb313a131add143773d7
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-25 18:40:31 +0300

    Added return for filter methods

--- src/AllTasks.js
+++ src/AllTasks.js
@@ -56,6 +56,9 @@ const AllTasks = (function () {
     updateTask,
     getTasks,
     updateTaskDueDate,
+    getOverdueTasks,
+    getTodayTasks,
+    getThisWeekTasks,
     startWithTestTasks,
   };
 })();
