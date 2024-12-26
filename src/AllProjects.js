8a61df953a462df03a94280959254555ea23a042
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-12-26 13:55:53 +0300

    Created AllProjects file
    This module will operate the logic of the projects (adding, updating etc)

---
+++ src/AllProjects.js
@@ -0,0 +1,25 @@
+import Project from "./project";
+
+const AllProjects = (function () {
+  const projects = [];
+
+  const addProject = (project) => projects.push(project);
+  const removeProject = (index) => projects.splice(index, 1);
+  const getProjects = () => [...projects];
+  const getProjectByName = (name) =>
+    projects.find((project) => project.name === name);
+
+  const startWithDefaultProject = () => {
+    projects.push(new Project("Default"));
+  };
+
+  return {
+    addProject,
+    removeProject,
+    getProjects,
+    getProjectByName,
+    startWithDefaultProject,
+  };
+})();
+
+export default AllProjects;
