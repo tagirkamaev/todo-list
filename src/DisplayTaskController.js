120a8ea5ac1528f4cca47d3cc7698b73b22162c4
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-01-07 10:09:24 +0300

    Updated quotes (due to using new settings for prettier)

--- src/DisplayTaskController.js
+++ src/DisplayTaskController.js
@@ -1,124 +1,121 @@
-import AllTasks from "./AllTasks";
+import AllTasks from './AllTasks'
 
 const DisplayTaskController = (function () {
   const renderTasks = function (tasks) {
-    const taskContainer = document.getElementById("tasks");
-    taskContainer.innerHTML = "";
+    const taskContainer = document.getElementById('tasks')
+    taskContainer.innerHTML = ''
 
     // display tasks
     tasks.forEach((task, index) => {
-      const taskCard = document.createElement("div");
-      taskCard.classList.add("todo");
+      const taskCard = document.createElement('div')
+      taskCard.classList.add('todo')
 
       // attaching class depending on priority
-      if (task.priority === "high") {
-        taskCard.classList.add("priority-high");
-      } else if (task.priority === "medium") {
-        taskCard.classList.add("priority-medium");
-      } else if (task.priority === "low") {
-        taskCard.classList.add("priority-low");
+      if (task.priority === 'high') {
+        taskCard.classList.add('priority-high')
+      } else if (task.priority === 'medium') {
+        taskCard.classList.add('priority-medium')
+      } else if (task.priority === 'low') {
+        taskCard.classList.add('priority-low')
       }
 
       // checkbox
-      const checkboxContainer = document.createElement("div");
-      checkboxContainer.classList.add("checkbox");
-      const taskCheckbox = document.createElement("input");
-      taskCheckbox.setAttribute("type", "checkbox");
-      taskCheckbox.setAttribute("data-task-index", index);
-      taskCheckbox.checked = task.checklist;
-      checkboxContainer.appendChild(taskCheckbox);
+      const checkboxContainer = document.createElement('div')
+      checkboxContainer.classList.add('checkbox')
+      const taskCheckbox = document.createElement('input')
+      taskCheckbox.setAttribute('type', 'checkbox')
+      taskCheckbox.setAttribute('data-task-index', index)
+      taskCheckbox.checked = task.checklist
+      checkboxContainer.appendChild(taskCheckbox)
 
       // task content
-      const taskContent = document.createElement("div");
-      taskContent.classList.add("task-content");
+      const taskContent = document.createElement('div')
+      taskContent.classList.add('task-content')
 
-      const taskTitle = document.createElement("span");
-      taskTitle.classList.add("task-title");
-      taskTitle.textContent = task.title;
+      const taskTitle = document.createElement('span')
+      taskTitle.classList.add('task-title')
+      taskTitle.textContent = task.title
 
-      const taskDueDate = document.createElement("span");
-      taskDueDate.classList.add("task-due-date");
-      taskDueDate.textContent = task.dueDate ? task.formatDueDate() : "";
+      const taskDueDate = document.createElement('span')
+      taskDueDate.classList.add('task-due-date')
+      taskDueDate.textContent = task.dueDate ? task.formatDueDate() : ''
 
-      taskDueDate.addEventListener("click", () => {
-        const newDate = prompt(
-          "Enter new due date (dd-mm-yyyy):",
-          task.dueDate
-        );
+      taskDueDate.addEventListener('click', () => {
+        const newDate = prompt('Enter new due date (dd-mm-yyyy):', task.dueDate)
         if (newDate) {
-          AllTasks.updateTaskDueDate(index, newDate);
-          task.dueDate.textContent = task.formatDueDate();
+          AllTasks.updateTaskDueDate(index, newDate)
+          task.dueDate.textContent = task.formatDueDate()
         }
-      });
+      })
 
-      taskContent.appendChild(taskTitle);
-      taskContent.appendChild(taskDueDate);
+      taskContent.appendChild(taskTitle)
+      taskContent.appendChild(taskDueDate)
 
       // delete button
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "Delete";
-      deleteButton.classList.add("delete-task");
+      const deleteButton = document.createElement('button')
+      deleteButton.textContent = 'Delete'
+      deleteButton.classList.add('delete-task')
 
-      taskCard.appendChild(checkboxContainer);
-      taskCard.appendChild(taskContent);
-      taskCard.appendChild(deleteButton);
+      taskCard.appendChild(checkboxContainer)
+      taskCard.appendChild(taskContent)
+      taskCard.appendChild(deleteButton)
 
-      taskCard.addEventListener("click", () => {
-        if (typeof onTaskSelected === "function") {
-          onTaskSelected(index);
+      taskCard.addEventListener('click', () => {
+        if (typeof onTaskSelected === 'function') {
+          onTaskSelected(index)
         }
-      });
+      })
 
-      taskContainer.appendChild(taskCard);
+      taskContainer.appendChild(taskCard)
 
       // callback for events
-      deleteButton.addEventListener("click", () => {
-        if (typeof onDelete === "function") onDelete(index);
-      });
+      deleteButton.addEventListener('click', () => {
+        if (typeof onDelete === 'function') onDelete(index)
+      })
 
-      taskCheckbox.addEventListener("change", () => {
-        if (typeof onToggle === "function")
-          onToggle(index, taskCheckbox.checked);
-      });
-    });
-  };
+      taskCheckbox.addEventListener('change', () => {
+        if (typeof onToggle === 'function')
+          onToggle(index, taskCheckbox.checked)
+      })
+    })
+  }
 
   const renderTaskDetails = (task) => {
-    const detailsContainer = document.getElementById("task-details");
+    const detailsContainer = document.getElementById('task-details')
 
-    detailsContainer.innerHTML = "";
+    detailsContainer.innerHTML = ''
 
-    const priorityFlag = document.createElement("i");
-    priorityFlag.classList.add("priority-flag");
-    priorityFlag.classList.add("fa-solid", "fa-flag");
+    const priorityFlag = document.createElement('i')
+    priorityFlag.classList.add('priority-flag')
+    priorityFlag.classList.add('fa-solid', 'fa-flag')
     priorityFlag.style.color =
-      task.priority === "high"
-        ? "#e74c3c"
-        : task.priority === "medium"
-        ? "#f1c40f"
-        : "#2ecc71";
+      task.priority === 'high'
+        ? '#e74c3c'
+        : task.priority === 'medium'
+          ? '#f1c40f'
+          : '#2ecc71'
 
-    const checkboxInDetails = document.createElement("input");
-    checkboxInDetails.type = "checkbox";
-    checkboxInDetails.classList.add("checkbox-details");
-    checkboxInDetails.addEventListener("change", () => {
-      AllTasks.updateTaskChecklist;
-    });
+    const checkboxInDetails = document.createElement('input')
+    checkboxInDetails.type = 'checkbox'
+    checkboxInDetails.classList.add('checkbox-details')
+    checkboxInDetails.addEventListener('change', () => {
+      AllTasks.updateTaskChecklist
+    })
 
-    const dueDateInDetailsContainer = document.createElement("div");
-    const dueDateIcon = document.createElement("i");
-    dueDateIcon.classList.add("date-details");
-    dueDateIcon.classList.add("fa-solid", "fa-calendar");
-    dueDateInDetailsContainer.appendChild(dueDateIcon);
+    const dueDateInDetailsContainer = document.createElement('div')
+    const dueDateIcon = document.createElement('i')
+    dueDateIcon.classList.add('date-details')
+    dueDateIcon.classList.add('fa-solid', 'fa-calendar')
+    dueDateInDetailsContainer.appendChild(dueDateIcon)
     if (task.dueDate) {
-      const dueDateInDetails = document.createElement("span");
-      dueDateInDetails.textContent = task.dueDate;
-      dueDateInDetailsContainer.appendChild(dueDateInDetails);
+      const dueDateInDetails = document.createElement('span')
+      dueDateInDetails.textContent = task.dueDate
+      dueDateInDetailsContainer.appendChild(dueDateInDetails)
     }
 
-    detailsContainer.appendChild(dueDateInDetailsContainer);
-    detailsContainer.appendChild(checkboxInDetails);
-    detailsContainer.appendChild(priorityFlag);
+    detailsContainer.appendChild(dueDateInDetailsContainer)
+    detailsContainer.appendChild(checkboxInDetails)
+    detailsContainer.appendChild(priorityFlag)
     // update title
     // const titleField = detailsContainer.querySelector("#details-title");
     // titleField.addEventListener("input", () => {
@@ -144,24 +141,24 @@ const DisplayTaskController = (function () {
     //     onUpdateDate(task.index, task.dueDate);
     //   }
     // });
-  };
+  }
 
-  let onDelete = null;
-  let onToggle = null;
-  let onTaskSelected = null;
-  let onUpdateTitle = null;
-  let onUpdateNotes = null;
-  let onUpdateDate = null;
+  let onDelete = null
+  let onToggle = null
+  let onTaskSelected = null
+  let onUpdateTitle = null
+  let onUpdateNotes = null
+  let onUpdateDate = null
 
   const setCallbacks = (callbacks) => {
-    onDelete = callbacks.onDelete;
-    onToggle = callbacks.onToggle;
-    onTaskSelected = callbacks.onTaskSelected;
-    onUpdateNotes = callbacks.onUpdateNotes;
-    onUpdateTitle = callbacks.onUpdateTitle;
-  };
+    onDelete = callbacks.onDelete
+    onToggle = callbacks.onToggle
+    onTaskSelected = callbacks.onTaskSelected
+    onUpdateNotes = callbacks.onUpdateNotes
+    onUpdateTitle = callbacks.onUpdateTitle
+  }
 
-  return { renderTasks, renderTaskDetails, setCallbacks };
-})();
+  return { renderTasks, renderTaskDetails, setCallbacks }
+})()
 
-export default DisplayTaskController;
+export default DisplayTaskController
