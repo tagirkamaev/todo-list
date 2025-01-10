5e3c5e22364ac94455b506fdcd8fed0b2b4492c9
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-01-10 17:39:24 +0300

    Created default project 'Inbox' and updated addProject method

--- src/AllProjects.js
+++ src/AllProjects.js
@@ -3,7 +3,14 @@ import Project from './project'
 const AllProjects = (function () {
   const projects = []
 
-  const addProject = (project) => projects.push(project)
+  const defaultProject = new Project('Inbox')
+  projects.push(defaultProject)
+
+  const addProject = (project) => {
+    if (!projects.includes(project)) {
+      projects.push(project)
+    }
+  }
   const removeProject = (index) => projects.splice(index, 1)
   const getProjects = () => [...projects]
   const getProjectByName = (name) =>
