327e17238087e79c79e5817bea0eb7ce26ad7771
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-01-11 19:32:43 +0300

    Deleted Tasks array and methods

--- src/project.js
+++ src/project.js
@@ -1,25 +1,6 @@
 class Project {
   constructor(name) {
     this.name = name
-    this.tasks = []
-  }
-
-  addTask(task) {
-    if (!this.tasks.includes(task)) {
-      this.tasks.push(task)
-      task.project = this
-    }
-  }
-
-  removeTask(task) {
-    this.tasks = this.tasks.filter((t) => t !== task)
-    if (task.project === this) {
-      task.project = null
-    }
-  }
-
-  getTasks() {
-    return [...this.tasks]
   }
 }
 
