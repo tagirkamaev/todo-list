ebf5aa272321a2359c23c07bfdce54b299d09a0e
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2025-01-15 10:04:58 +0300

    Added eventListener to options button, fixed scenario when clicking outside task input

--- src/AddTaskController.js
+++ src/AddTaskController.js
@@ -1,11 +1,15 @@
 import 'flatpickr/dist/flatpickr.min.css'
 import AllTasks from './AllTasks'
+import AllProjects from './AllProjects'
 
 const AddTaskController = (function () {
   const taskInput = document.getElementById('task-input')
   const taskOptions = document.getElementById('task-options')
   const taskDateIcon = document.getElementById('task-date-icon')
+  const taskOptionsIcon = document.getElementById('task-more-options-button')
+  const optionsContainer = document.getElementById('options-container')
   const taskPriority = document.getElementById('task-priority')
+  const taskProject = document.getElementById('task-project')
 
   const setupEventListeners = (onTaskAdd) => {
     // showing icons after clicking input
@@ -14,12 +18,16 @@ const AddTaskController = (function () {
     })
 
     // concealing additional elements
-    taskInput.addEventListener('blur', () => {
-      setTimeout(() => {
-        if (taskInput.value.trim() === '') {
-          taskOptions.classList.add('hidden')
-        }
-      }, 200)
+    document.addEventListener('click', (event) => {
+      if (
+        !taskOptions.contains(event.target) &&
+        !optionsContainer.contains(event.target) &&
+        event.target !== taskInput &&
+        event.target !== taskOptionsIcon
+      ) {
+        taskOptions.classList.add('hidden')
+        optionsContainer.classList.add('hidden')
+      }
     })
 
     // adding task after enter
@@ -28,16 +36,18 @@ const AddTaskController = (function () {
         const title = taskInput.value.trim()
         const priority = taskPriority.value
         const dueDate = taskInput.dataset.dueDate || null
+        const project = taskProject.value
 
-        onTaskAdd(title, dueDate, priority)
+        onTaskAdd(title, dueDate, priority, project)
 
         taskInput.value = ''
         taskOptions.classList.add('hidden')
+        optionsContainer.classList.add('hidden')
       }
     })
 
     // date pick
-    taskDateIcon.addEventListener('click', () => {
+    taskDateIcon.addEventListener('focus', () => {
       const dateInput = document.createElement('input')
       dateInput.type = 'text'
       dateInput.classList.add('date-picker')
@@ -49,6 +59,20 @@ const AddTaskController = (function () {
       })
 
       dateInput.focus()
+    })
+
+    // task options button
+    taskOptionsIcon.addEventListener('focus', () => {
+      optionsContainer.classList.remove('hidden')
+    })
+
+    // projects select
+    const projects = AllProjects.getProjects()
+    projects.forEach((project) => {
+      const option = document.createElement('option')
+      option.value = project.name
+      option.textContent = project.name
+      taskProject.appendChild(option)
     })
   }
 
